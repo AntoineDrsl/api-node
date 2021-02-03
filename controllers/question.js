9c9a737a3bcd99f758c4058591d429ec3939be39
Nicolas <nicobrazzo@gmail.com>
2021-02-03 16:07:03 +0100

    ADD CRUD quizzs

--- controllers/question.js
+++ controllers/question.js
@@ -1,7 +1,86 @@
+const {dbConfig} = require('../knexfile');
+const knex = require('knex')(dbConfig.development);
+
 class Question {
 
-    static test() {
-        console.log('coucou');
+    static createQuizz(req, res) {
+
+        const error = [];
+
+        if(!req.body.theme || req.body.theme.length === 0) {
+            error.push("Veuillez entrer un nom de theme valide");
+        }
+
+        if(error.length > 0) {
+            return res.status(417).json({status: false, error: error[0]})
+        }
+
+        knex('quizzs').insert({
+            theme: req.body.theme
+        }).then((quizz) => {
+            if(quizz) {
+                return res.status(201).json({status: true, quizz: quizz})
+            }
+            else {
+                return res.status(403).json({status: false, message: 'Votre quizz n\'a pas pu être créé'});
+            }
+        });
+    }
+
+    static getAllQuizz(req, res) {
+        knex('quizzs').then((quizz) => {
+            if(quizz) {
+                return res.status(200).json({status: true, quizz: quizz})
+            }
+            else {
+                return res.status(404).json({status: false, message: 'Aucun quizz n\'a été créé'});
+            }
+        })
+    }
+
+    static getQuizzById(req, res) {
+        knex('quizzs').where({id: req.params.id})
+        .then((quizz) => {
+            if(quizz) {
+                return res.status(200).json({status: true, quizz: quizz})
+            }
+            else {
+                return res.status(404).json({status: false, message: 'Aucun quizz n\'a été créé'});
+            }
+        })
+    }
+
+    static UpdateQuizzById(req, res) {
+        const error = [];
+
+        if(!req.body.theme || req.body.theme.length === 0) {
+            error.push("Veuillez entrer un nom de theme valide");
+        }
+
+        if(error.length > 0) {
+            return res.status(417).json({status: false, error: error[0]})
+        }
+
+
+        knex('quizzs').where({id: req.params.id})
+        .update({theme: req.body.theme})
+        .then((quizz) => {
+            if(quizz) {
+                return res.status(200).json({status: true, quizz: quizz})
+            }
+            else {
+                return res.status(404).json({status: false, message: 'Aucun quizz n\'a été créé'});
+            }
+        })
+    }
+
+    static RemoveQuizzById(req, res) {
+
+        knex('quizzs').where({id: req.params.id})
+        .del()
+        .then(() => {
+            return res.status(200).json({status: true, message: 'Le quizz a bien été supprimé'});
+        });
     }
 
 }
--- routes/question.route.js
+++ routes/question.route.js
@@ -2,6 +2,11 @@ const express = require('express');
 const Question = require('../controllers/question');
 const questionRoute = express.Router();
 
-questionRoute.get('/', Question.test);
+questionRoute.get('/', Question.getAllQuizz);
+questionRoute.get('/:id', Question.getQuizzById);
+
+questionRoute.post('/', Question.createQuizz);
+questionRoute.put('/:id', Question.UpdateQuizzById);
+questionRoute.delete('/:id', Question.RemoveQuizzById);
 
 module.exports = questionRoute;
